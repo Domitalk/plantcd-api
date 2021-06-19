0d90a92a5912758d0515b3003c2e50e2957c84f9
Dominic Chu <dominic.m.chu@gmail.com>
2021-06-19 13:26:49 -0700

    middleware and persistence fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def create 
         @user = User.create(user_params)
-        render json: @user.to_json
+        render json: @user
     end 
 
     def show 
         @user = User.find(params[:id])
         render json: @user.to_json
+    end 
+
+    def index 
+        @users = User.all 
+        render json: @users.to_json 
     end 
 
     private 
