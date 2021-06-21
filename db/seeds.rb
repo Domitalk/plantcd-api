614987a4c5bea6f3e4b631ff8b74d68bafed1bb0
Dominic Chu <dominic.m.chu@gmail.com>
2021-06-21 14:54:18 -0700

    pcreate password digest and has_secure_password

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,13 @@ User.create(
     user_name: 'user_name_1',
     first_name: 'first_name_1',
     middle_name: 'middle_name_1',
-    last_name: 'last_name_1'
+    last_name: 'last_name_1',
+    password: '123'
 )
 User.create(
     user_name: 'user_name_2',
     first_name: 'first_name_2',
     middle_name: 'middle_name_2',
-    last_name: 'last_name_2'
+    last_name: 'last_name_2',
+    password: '123'
 )
